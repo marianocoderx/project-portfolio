529974eb556837ef3d4e4433e530827902f04c96
Mariano <marianobugallo100@gmail.com>
2023-12-06 17:56:56 -0300

    new commit

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,66 +1,96 @@
 import React from "react";
 import "../stylesheets/services.css";
-import CardComp from "../subcomponents/card";
+import { Button, Card } from "react-bootstrap";
 
 import Service1 from "../images/service1.png";
 import Service2 from "../images/service2.png";
 import Service3 from "../images/service3.png";
 import Service4 from "../images/service4.png";
 
-export default function ServicesComp() {
-  // Array de datos para cada servicio
-  const servicesData = [
-    {
-      title: "Mantenimiento",
-      image: Service1,
-      buttonText: "Ver más",
-    },
-    {
-      title: "Desarrollo y diseño",
-      image: Service2,
-      buttonText: "Ver más",
-    },
-    {
-      title: "Gestion de base de datos",
-      image: Service3,
-      buttonText: "Ver más",
-    },
-    {
-      title: "Desarrollo de API's",
-      image: Service4,
-      buttonText: "Ver más",
-    },
-  ];
+const servicesData = [
+  {
+    title: "Mantenimiento",
+    image: Service1,
+  },
+  {
+    title: "Desarrollo y diseño",
+    image: Service2,
+  },
+  {
+    title: "Gestion de base de datos",
+    image: Service3,
+  },
+  {
+    title: "Desarrollo de API's",
+    image: Service4,
+  },
+];
 
+export default function ServicesComp() {
   return (
     <>
-    <div className="mainContainer">
-      <div className="mainContainerText">
-        <h1>Lorem ipsum</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, soluta?</p>
-      </div>
-  
-      <div className="containerServices">
-        <div className="gridContainerx">
-          {/* Mapeo a través del array de servicios y renderización de la tarjeta para cada uno */}
+      <div className="mainContainer">
+        <div className="mainContainer-text">
+          <h1>Necesitas algo? Puedo ayudarte...</h1>
+          <p>Soluciones a medida a tus necesidades.</p>
+        </div>
+
+        <div className="mainContainer-body">
+
           {servicesData.map((service, index) => (
-            <div key={index} className="gridItem">
-              <div className="imageContainer">
-                <CardComp
-                  title={service.title}
-                  imageUrl={service.image}
-                  buttonText={service.buttonText}
-                  onButtonClick={() => console.log(`Botón clickeado para ${service.title}`)}
-                  imageWidth={110}
-                  imageHeight={110}
-                />
-              </div>
+
+            <div className="mainContainer-grid">
+
+              <Card>
+
+                  <div className="mainContainer-grid-img">
+
+                    <Card.Img variant="top" src={service.image} />
+
+                  </div>
+
+                <Card.Body>
+
+                  <div className="mainContainer-grid-text">
+
+                     <Card.Title>{service.title}</Card.Title>
+
+                  </div>
+
+
+                  <div className="mainContainer-grid-button">
+
+                    <Button 
+                      variant="primary" 
+                      style={{ 
+                        backgroundColor:'transparent',
+                        border:'2px solid aliceblue',
+                        width: '150px', 
+                        height: '40px', 
+                        display: 'flex', 
+                        flexDirection: 'column', 
+                        justifyContent: 'center', 
+                        alignItems: 'center',
+                        margin: 'auto'  // Añade esta línea para centrar horizontalmente
+                      }}
+                    >
+                    Ver más
+                    </Button> 
+
+
+                  </div>
+
+                </Card.Body>
+
+              </Card>
+
             </div>
+
           ))}
+          
         </div>
       </div>
-    </div>
-  </>
+    </>
   );
 }
 
